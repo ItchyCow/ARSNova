4d07234fe583462f1e6573bf96fddf5ad37a3188
ItchyCow <94447343+ItchyCow@users.noreply.github.com>
2021-12-07 02:35:28 +0800

    updated to include profilepage

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,8 @@ module.exports = {
   entry: {
     index: './public/src/index.js',
     homepage: './public/src/homepage.js',
+    editprofile: './public/src/editprofile.js',
+    profilepage: './public/src/profilepage.js'
   },
   output: {
     path: path.resolve(__dirname, 'public'),
